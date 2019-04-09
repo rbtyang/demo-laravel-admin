5922b198c9d4094ec6fe77ed6b61409a24b5f5eb
fumingyang <robotyang123@163.com>
2019-04-09 10:13:36 +0800

    Revert: Merge branch 'master' of github.com:rbtyang/demo-laravel-admin

---
+++ app/Admin/routes.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+Admin::registerAuthRoutes();
+
+Route::group([
+    'prefix'     => config('admin.route.prefix'),
+    'namespace'  => config('admin.route.namespace'),
+    'middleware' => config('admin.route.middleware'),
+], function (Router $router) {
+    $router->get('/', 'HomeController@index');
+    $router->any('auth/store/', 'AuthController@store');
+
+    $router->resource('demo/users', UserController::class);
+    $router->resource('product/uom', UomController::class);
+    $router->resource('product/category', CategoryController::class);
+    $router->resource('product/stock', StockController::class);
+    $router->resource('product/price', PriceController::class);
+});
